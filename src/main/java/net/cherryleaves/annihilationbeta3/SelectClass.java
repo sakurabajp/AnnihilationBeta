4e336a3dfc9dc90cb31dc5e4d463bcd839cc68f2
sakuraba <sakuraba8050@gmail.com>
2024-12-23 23:05:34 +0900

    クラス追加

--- src/main/java/net/cherryleaves/annihilationbeta3/SelectClass.java
+++ src/main/java/net/cherryleaves/annihilationbeta3/SelectClass.java
@@ -2,6 +2,7 @@ package net.cherryleaves.annihilationbeta3;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.Inventory;
@@ -16,7 +17,28 @@ public class SelectClass implements Listener {
 
     public void ClassList(Player p){
         List<ItemStack> Classes = new ArrayList<>();
+        ListAddClass(Classes);
         Inventory cg = Bukkit.createInventory(null, 45, sc);
         p.openInventory(cg);
+    }
+
+    public void ListAddClass(List<ItemStack> Classes) {
+        Classes.add(new GUI().ItemMeta(Material.FEATHER, "Acrobat"));
+        Classes.add(new GUI().ItemMeta(Material.BREWING_STAND, "Alchemist"));
+        Classes.add(new GUI().ItemMeta(Material.BOW, "Archer"));
+        Classes.add(new GUI().ItemMeta(Material.POTION, "Assassin"));
+        Classes.add(new GUI().ItemMeta(Material.JUKEBOX, "Bard"));
+        Classes.add(new GUI().ItemMeta(Material.CHAINMAIL_CHESTPLATE, "Berserker"));
+        Classes.add(new GUI().ItemMeta(Material.FERMENTED_SPIDER_EYE, "Bloodmage"));
+        Classes.add(new GUI().ItemMeta(Material.BRICK, "Builder"));
+        Classes.add(new GUI().ItemMeta(Material.CRAFTING_TABLE, "Civilian"));
+        Classes.add(new GUI().ItemMeta(Material.ENDER_PEARL, "Dasher"));
+        Classes.add(new GUI().ItemMeta(Material.PRISMARINE, "Defender"));
+        Classes.add(new GUI().ItemMeta(Material.ENCHANTING_TABLE, "Enchanter"));
+        Classes.add(new GUI().ItemMeta(Material.TNT, "Engineer"));
+        Classes.add(new GUI().ItemMeta(Material.WHEAT_SEEDS, "Farmer"));
+        Classes.add(new GUI().ItemMeta(Material.ANVIL, "HandyMan"));
+        Classes.add(new GUI().ItemMeta(Material.CHORUS_FRUIT, "Healer"));
+        // はぁ...
     }
 }
